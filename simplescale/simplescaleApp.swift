0374c8cc678f1b59ede95981d00ae7902912ce90
Jason Ting <jzting@gmail.com>
2026-06-26 15:40:41 -0400

    Initial Commit

---
+++ simplescale/simplescaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplescaleApp.swift
+//  simplescale
+//
+//  Created by Jason Ting on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct simplescaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
